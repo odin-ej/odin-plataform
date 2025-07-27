4b8e74dbbd0056a5319e02b4ceb84d76b425144e
Yan Ferreira <fabioyan2@gmail.com>
2025-07-27 13:25:05 -0300

    feat: add loading.tsx at root layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,15 @@ module.exports = {
         blueBrand: "#0126fB", // substitua com o valor real da sua constante BLUE
         darkblueBrand: "#00205e", // substitua com o valor real da sua constante BLUE
       },
+      animation: {
+        'spin-reverse': 'spin-reverse 1s linear infinite', // Define a duração e o tipo de animação
+      },
+      keyframes: {
+        'spin-reverse': {
+          from: { transform: 'rotate(360deg)' }, // Começa em 360 para girar ao contrário
+          to: { transform: 'rotate(0deg)' },    // Termina em 0
+        }
+      }
     },
   },
   plugins: [],
