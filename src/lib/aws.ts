ae643fdbaf64e22f2b55d645633dca0fd996ccd5
Yan Ferreira <fabioyan2@gmail.com>
2025-07-27 20:52:33 -0300

    fix: credentials aws users

--- src/lib/aws.ts
+++ src/lib/aws.ts
@@ -2,18 +2,15 @@ import { CognitoIdentityProviderClient } from "@aws-sdk/client-cognito-identity-
 import { S3Client } from "@aws-sdk/client-s3";
 import { SESClient } from "@aws-sdk/client-ses";
 
-export const cognitoClient = new CognitoIdentityProviderClient({
-  region: process.env.REGION,
-});
-
-export const s3Client = new S3Client({
+const awsClientConfig = {
   region: process.env.REGION,
   credentials: {
     accessKeyId: process.env.ACCESS_KEY_ID!,
     secretAccessKey: process.env.SECRET_ACCESS_KEY!,
   },
-});
+};
 
-export const sesClient = new SESClient({
-  region: process.env.REGION,
-});
+// Inicializa todos os clientes com a mesma configuração explícita
+export const cognitoClient = new CognitoIdentityProviderClient(awsClientConfig);
+export const s3Client = new S3Client(awsClientConfig);
+export const sesClient = new SESClient(awsClientConfig);
