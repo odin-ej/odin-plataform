5bcea8ed27233034ac8da84569fa427bcaf9316a
Yan Ferreira <fabioyan2@gmail.com>
2025-07-27 21:12:18 -0300

    fix: add credentials to aws

--- src/lib/aws.ts
+++ src/lib/aws.ts
@@ -4,6 +4,10 @@ import { SESClient } from "@aws-sdk/client-ses";
 
 const awsClientConfig = {
   region: process.env.AWS_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID as string,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY as string,
+  },
 };
 
 // Inicializa todos os clientes com a mesma configuração explícita
