471522997ce5b2cad2388e628219a648b02804f4
Yan Ferreira <fabioyan2@gmail.com>
2025-08-02 14:20:11 -0300

    style: change 3 cols to 2 cols on usefullinkssection

--- src/app/_components/Dashboard/UsefulLinksSection.tsx
+++ src/app/_components/Dashboard/UsefulLinksSection.tsx
@@ -295,7 +295,7 @@ const UsefulLinksSection = ({
         </div>
       </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4">
         {paginatedLinks.map((link) => (
           <div
             key={link.id}
