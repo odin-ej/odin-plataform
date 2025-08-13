e02e24b31efbb64bddf154f64e2dfa4ccf10dfbc
Yan Ferreira <fabioyan2@gmail.com>
2025-08-13 00:33:14 -0300

    fix: ensure that title is optional

---
+++ prisma/migrations/20250813033021_add_interrogation_to_title/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "RoomReservation" ALTER COLUMN "title" DROP NOT NULL;
--- src/app/_components/Dashboard/RoomsContent.tsx
+++ src/app/_components/Dashboard/RoomsContent.tsx
@@ -216,7 +216,7 @@ const RoomsContent = ({
   ): FlattenedReservation[] => {
     return data.map((res) => ({
       id: res.id,
-      title: res.title,
+      title: res.title as string ?? 'Sem título',
       roomName: res.room.name,
       userName: res.user.name,
       formattedDate:
@@ -256,7 +256,7 @@ const RoomsContent = ({
       setEditingReservation(reservation);
       form.reset({
         date: format(new Date(reservation.date), "yyyy-MM-dd"),
-        title: reservation.title,
+        title: reservation.title as string ?? 'Sem título',
         hourEnter: format(new Date(reservation.hourEnter), "HH:mm"),
         hourLeave: format(new Date(reservation.hourLeave), "HH:mm"),
         roomId: reservation.roomId,
