a8bd89aae9d0a63c810ad51b2da27f346e66d344
Yan Ferreira <fabioyan2@gmail.com>
2025-08-17 19:35:52 -0300

    refactor: remove Interesses label

--- src/app/_components/Dashboard/usuarios/ProfessionalInterestsManager.tsx
+++ src/app/_components/Dashboard/usuarios/ProfessionalInterestsManager.tsx
@@ -1,6 +1,11 @@
 "use client";
 
-import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 import CustomCheckboxGroup from "../../Global/Custom/CustomCheckboxGroup";
 import { InterestCategory, ProfessionalInterest } from "@prisma/client";
 import { useFormContext } from "react-hook-form";
@@ -13,24 +18,36 @@ interface ProfessionalInterestsManagerProps {
   interestCategories: InterestsWithCategory[];
 }
 
-
-const ProfessionalInterestsManager = ({ interestCategories }: ProfessionalInterestsManagerProps) => {
+const ProfessionalInterestsManager = ({
+  interestCategories,
+}: ProfessionalInterestsManagerProps) => {
   const { control } = useFormContext();
   return (
     <div className="space-y-4 p-4 border border-dashed border-gray-600 rounded-lg">
-      <h3 className="font-semibold text-lg text-[#f5b719]">Interesses Profissionais</h3>
-      <p className="text-sm text-gray-400">Selecione as áreas que mais te interessam. Isso nos ajuda a direcionar oportunidades e conteúdos relevantes para você.</p>
-      
+      <h3 className="font-semibold text-lg text-[#f5b719]">
+        Interesses Profissionais
+      </h3>
+      <p className="text-sm text-gray-400">
+        Selecione as áreas que mais te interessam. Isso nos ajuda a direcionar
+        oportunidades e conteúdos relevantes para você.
+      </p>
+
       <Accordion type="multiple" className="w-full">
-        {interestCategories.map(category => (
-          <AccordionItem key={category.id} value={category.id} className="border-b-gray-700">
-            <AccordionTrigger className="font-semibold hover:no-underline text-white">{category.name}</AccordionTrigger>
+        {interestCategories.map((category) => (
+          <AccordionItem
+            key={category.id}
+            value={category.id}
+            className="border-b-gray-700"
+          >
+            <AccordionTrigger className="font-semibold hover:no-underline text-white">
+              {category.name}
+            </AccordionTrigger>
             <AccordionContent className="pt-2">
               <CustomCheckboxGroup
-              label='Interesses'
+                label=""
                 control={control}
                 name={"professionalInterests"}
-                options={category.interests.map(interest => ({
+                options={category.interests.map((interest) => ({
                   value: interest.id,
                   label: interest.name,
                 }))}
